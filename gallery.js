b007da2603aa46920a3c58b6c6148837be77b95e
PavaniVutukuri <138871847+PavaniVutukuri@users.noreply.github.com>
2023-07-23 22:49:59 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,15 @@
+var img=document.images;
+console.log(img)
+for(i=0;i<10;i++)
+{
+    img[i].src=`${i}.jpg`
+}
+function bigImageWindow(urlOfImg)
+{
+    document.getElementById("bigImage").style.display="flex"
+    document.getElementById("Image").style.backgroundImage=`url(${urlOfImg})`
+}
+function closeWindow()
+{
+    document.getElementById("bigImage").style.display="none"
+}
